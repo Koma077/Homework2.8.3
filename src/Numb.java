fd2379799b3561857545c0ff81a87fe2023b2d26
Koma077 <Sladkoeshka33@yandex.ru>
2022-11-02 22:50:22 +0700

    XXXXXXX

--- src/Numb.java
+++ src/Numb.java
@@ -1,32 +1,27 @@
 import java.util.*;
 
 public class Numb {
-//    public static void main(String[] args) {
-//        Set<Integer> newSet = new HashSet<>();
-//        for (int i = 0; i < 20; i++) {
-//            newSet.add((int) (Math.random() * 1000));
-//        }
-//        newSet.removeIf(number -> number % 2 == 1);
-//        System.out.println(newSet);
-//
-//    }
-//}
-    public static Set<Integer> randomNumbers() {
-        Set<Integer> integers = new HashSet<>();
-        for (int i = 0; i < 20; i++) {
-            double random = Math.random() * 1000;
-            integers.add((int) random);
-        }
-        integers.removeIf(integer -> integer % 2 == 1);
-        System.out.println(integers);
-        return integers;
-    }
+
 
 
     public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> number = new HashSet<>();
+        while (number.size() != 20) {
+            number.add(random.nextInt(1000));
+            number.removeIf(integer -> integer % 2 != 0);
+        }
+            System.out.println(number);
 
     }
 }
+
+
+
+
+
+
+
 
 
 
